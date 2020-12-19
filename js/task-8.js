66e70828ccdf3359b0010166782d75e462034cc4
myaryhin0201 <yarygin45@gmail.com>
2020-12-20 00:14:00 +0100

    add 8 task

--- js/task-8.js
+++ js/task-8.js
@@ -13,11 +13,33 @@ console.log(`Задание 8`);
 // и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
 
-const userInput = document.querySelector('#controls input');
+const userInput = document.querySelector('.amount');
 const render = document.querySelector('button[data-action="render"]');
 const destroy = document.querySelector('button[data-action="destroy"]');
-
-
+const boxesPlace = document.querySelector('#boxes');
+const boxSize = 30;
+const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
 const createBoxes = (amount) => {
-    return amount;
+    destroyBoxes();
+    amount = Number(amount.value);
+    const itemRef = [];
+    console.log(amount);
+    for (let i = 0; i < amount;i+=1){
+        const element = document.createElement('div');
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        const size = `${boxSize +10*i}px`;
+        element.style.width = `${size}`;
+        element.style.height =`${size}`;
+        element.style.backgroundColor = `rgb(${r},${g},${b})`;
+        itemRef[i] = element;
+    }
+    boxesPlace.append(...itemRef);
+}   
+const destroyBoxes = () => {
+    boxesPlace.innerHTML = "";
 }
+render.addEventListener('click', () => { createBoxes(userInput);});
+destroy.addEventListener('click',() =>{ destroyBoxes();});
+
